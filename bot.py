cea9cbac27289c442ca56c27b4fa69b22dc75907
kikitiki02 <kikimarsh.02@gmail.com>
2026-05-22 23:00:02 -0500

    clean initial commit (no secrets)

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+TOKEN= os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! <3")
+
+bot.run(TOKEN)
